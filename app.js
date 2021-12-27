2f932b7310ea9e7552b4dccfbf49c465bcf4dac0
Nop Wangboonkird <40882755+Nop-Wang@users.noreply.github.com>
2021-12-27 15:05:58 +0700

    upadetweb

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const path = require("path");
+
+app.use(express.static(path.join(__dirname,"/public/")))
 
 app.get("/",(req,res)=>{
 
-    res.send("Hello NopWangboonkird")
+    res.send("Hello NopWangboonkird=1")
 })
 
 app.listen(port,()=>{
